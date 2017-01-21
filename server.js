e85c65d61eeed64205da9b2ef5043a611ac214c9
Christian Pena <christianpena9@gmail.com>
2017-01-21 17:47:42 -0500

    Added custom message event

--- server.js
+++ server.js
@@ -7,8 +7,20 @@ var io = require('socket.io')(http);
 app.use(express.static(__dirname + '/public'));
 
 //tells when the user has connected to a socket
-io.on('connection', function() {
+io.on('connection', function(socket) {
     console.log('user connected via socket.io');
+
+    socket.on('message', function(message) {
+        console.log('Message Recieved: ' + message.text);
+        //io.emit sends the data to everyone, including the sender
+
+        //broadcast.emit sends the date just to everyone
+        socket.broadcast.emit('message', message);
+    });
+
+    socket.emit('message', {
+        text: 'Welcome to the chat application!'
+    });
 });
 
 http.listen(PORT, function() {
