de226b1148532371b5dea60d936383ee8d38d4c3
Christian Pena <christianpena9@gmail.com>
2017-01-23 19:42:09 -0500

    Add name support

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,8 @@
 var socket = io();
+var name = getQueryVariable('name') || 'Anonymous';
+var room = getQueryVariable('room');
+
+console.log(name + ' wants to join ' + room);
 
 socket.on('connect', function() {
     console.log("connected to socket.io server.");
@@ -10,7 +14,11 @@ socket.on('message', function(message) {
 
     var momentTimestamp = moment.utc(message.timestamp);
 
-    jQuery('.messages').append('<p><strong>'+ momentTimestamp.local().format('h:mma') + '</strong>: ' + message.text + '</p>');
+    $message = jQuery('.messages');
+    $message.append('<p><strong>' + message.name + ' ' + momentTimestamp.local().format('h:mma') + '</strong></p>');
+    $message.append('<p>' + message.text + '</p>');
+
+    //jQuery('.messages').append('<p><strong>'+ momentTimestamp.local().format('h:mma') + '</strong>: ' + message.text + '</p>');
 });
 
 // Handles submitting of new message
@@ -22,6 +30,7 @@ $form.on('submit', function(event) {
     $message = $form.find('input[name=message]');
 
     socket.emit('message', {
+        name: name,
         text: $message.val()
     });
 
