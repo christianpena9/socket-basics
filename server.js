2e2af89b87c88cdca79376a06415222041819619
Christian Pena <christianpena9@gmail.com>
2017-01-25 08:56:01 -0500

    Add support for chat rooms

--- server.js
+++ server.js
@@ -8,9 +8,21 @@ var now = moment();
 
 app.use(express.static(__dirname + '/public'));
 
+var clientInfo = {};
+
 //tells when the user has connected to a socket
 io.on('connection', function(socket) {
     console.log('user connected via socket.io');
+
+    socket.on('joinRoom', function(req) {
+        clientInfo[socket.id] = req;
+        socket.join(req.room);
+        socket.broadcast.to(req.room).emit('message',{
+            name: 'System',
+            text: req.name + ' has joined!',
+            timestamp: moment().valueOf()
+        });
+    });
 
     socket.on('message', function(message) {
         console.log('Message Recieved: ' + message.text);
@@ -19,7 +31,7 @@ io.on('connection', function(socket) {
         message.timestamp = moment().valueOf();
         console.log('Message Timestamp: ' + message.timestamp);
         //broadcast.emit sends the date just to everyone
-        io.emit('message', message);
+        io.to(clientInfo[socket.id].room).emit('message', message);
     });
 
     socket.emit('message', {
